3f2c84438b2b866993af5b6e4d1b5a07249817b9
JuanPabloRami <juanpalabloramirezavs@gmail.com>
2021-12-02 02:04:26 -0500

    primer commit : feat: se agregó la opcion de agregar negocios por tipo

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, session, url_for
-from werkzeug.utils import redirect
+from flask import Flask, render_template, request, session, url_for,redirect
 
-import smtplib
+
+"""import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from email.mime.text import MIMEText"""
 
 
 usuarios = {}
 
+negocios_usuarios = {"panaderia": {}, "moda": {} }
+
 app = Flask(__name__)
-#app.secret_key = "ajhsdg56dkgasdhbs"
+app.secret_key = "ajhsdg56dkgasdhbs"
 
 #/ hace referencia a la url base: http://127.0.0.1:5000/
 
@@ -23,6 +25,35 @@ def bienvenida():
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
+
+@app.route('/tiponegocio')
+def formulariotiponegocio():
+    
+    return render_template('tiponegocio.html')
+
+@app.route('/detallesnegocio')
+def formulariodetallesnegocio():
+    return render_template('creacionnegocionegocio.html')
+
+
+
+@app.route('/negociocreado/<negocio>')
+def creacionnegocio(negocio):
+    #aca verificamos si la cookie es valida y extraemos el correo
+    #del usuario
+    if 'user' in session:
+        #aca extraemos el email del usuario
+        correo = session['user']
+        #HACEMOS LA INSCRIPCION AL CURSO MEDIANTE SU correo
+        if negocio == "panaderia" or negocio == "moda":
+            negocios_usuarios[negocio]["correo"] = correo
+
+        else:
+            return "error, usted solo puede crear un negocio valido", 401
+        print("negocios creados:  ", negocios_usuarios)
+        nombreusuario = usuarios[correo]["primernombre"]
+        return "Señor {} Usted ha creado su nuevo negocio ".format(nombreusuario), 201 
+    return redirect('/login' , 'error , debe iniciar !!funciona!!sesion')
 
 
 
@@ -48,6 +79,7 @@ def registrohecho():
     print("usuarios registrados: ", usuarios)
     print("--------------------------------------------------------------------------------------------------------------------")
 
+    """
     #credenciales
     proveedor_correo = 'smtp.live.com: 587'
     remitente = 'Adsi2339490_@outlook.com'
@@ -59,22 +91,16 @@ def registrohecho():
     #autenticacion
     servidor.login(remitente, password)
     #mensaje 
-    mensaje = """"<table>
-    <h1>BIENVENIDO</h1>
-    <p>BIENVENIDO A TURISTAPP, {{primernombre}} {{segundonombre}}</p>
-    <p>nombre de usuario {{username}}</p>
-    <p>correo {{correo}} a aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa 30</p>
-
-
-    </table>"""
+    mensaje = """
+    """
     msg = MIMEMultipart()
     msg.attach(MIMEText(mensaje, 'html'))
     msg['From'] = remitente
     msg['To'] = 'juanpalabloramirezavs@gmail.com'
     msg['Subject'] = 'BIENVENIDO A TURISTAPP 2'
     servidor.sendmail(msg['From'] , msg['To'], msg.as_string())
+    """
     print('se ha enviado un correo a',usuarios[correo])
-    print(mensaje)
     return render_template('registrohecho.html')
     
 
@@ -94,8 +120,19 @@ def logueado():
             print('----------------------------------')
             print(correo,"se ha logueado")
             print('----------------------------------')
-            return render_template('logueohecho.html')
+            #para saludar al usuario extraemos el nombre
+            nombre = usuarios[correo]["username"]
+            #generamos cookie de sesion igual al correo de usuario
+            #tabien, podriamos hacerlo con el id de base de datos etc
+            #tambein, podriamos agregar mas valores a la cookie
+            session['user'] = correo
+            return render_template('perfil.html', nombre_usuario=nombre)
         return "Usuario o password incorrecto"
-    return "Esta cuenta no existe, registrese."
+    return "Esta cuenta no existe, registrese.", 401
+
+
+
+
+
 
 app.run(debug = True, port=5000)
